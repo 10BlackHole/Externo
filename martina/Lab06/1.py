fab1cd137a0df799900dcb7aa9ea34398b812cb6
10BlackHole <borjadiez1014@gmail.com>
2022-05-04 04:22:02 -0400

    se agregan las restricciones

--- martina/Lab06/1.py
+++ martina/Lab06/1.py
@@ -8,9 +8,14 @@ def divisores(P):
 
 # 'N' cantidad de números a ingresar y 'P' guarda los números ingresados
 N = int(input("Ingrese la cantidad de números a considerar: "))
+while (N <= 0 or N > 100):          # Restricción para N
+    print("Error, ingrese un número en el intervalo (0, 100]")
+    N = int(input("Ingrese la cantidad de números a considerar: "))
 P = []
 for i in range(0, N):
     numeros = int(input("Ingrese un número: "))
+    while (numeros <= 0):        # Restricción para P
+        numeros = int(input("Error, ingrese un número mayor a 0: "))
     P.append(numeros)
 
 # Iteracion para  ver si 'P' es número perfecto o no

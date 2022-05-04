25762a76f8a2844f5fb1059fe280618dc3a0f980
10BlackHole <borjadiez1014@gmail.com>
2022-05-04 04:04:46 -0400

    lab06

---
+++ martina/Lab06/1.py
@@ -0,0 +1,24 @@
+# Definimos una función que nos entrega los divisores de 'P'
+def divisores(P):
+    divisores = []
+    for i in range(1, P):
+        if P%i == 0:
+            divisores.append(i)
+    return divisores
+
+# 'N' cantidad de números a ingresar y 'P' guarda los números ingresados
+N = int(input("Ingrese la cantidad de números a considerar: "))
+P = []
+for i in range(0, N):
+    numeros = int(input("Ingrese un número: "))
+    P.append(numeros)
+
+# Iteracion para  ver si 'P' es número perfecto o no
+for i in range(0, N):
+    suma_divisores = 0
+    for j in range(0, len(divisores(P[i]))):
+        suma_divisores = suma_divisores + divisores(P[i])[j]
+    if (suma_divisores == P[i]):
+        print(P[i], " Es un número perfecto")
+    else:
+        print(P[i], " No es un número perfecto")
